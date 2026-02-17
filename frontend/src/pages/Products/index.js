6e35b4a60c94cbed8c899c4629a44ec82d727c52
Saurabh Sarve <saurabhsarve25@gmail.com>
2026-02-17 22:38:19 +0530

    updated Products

---
+++ frontend/src/pages/Products/index.js
@@ -0,0 +1,7 @@
+export { default as ProductsPage } from './ProductsPage';
+export { default as ProductDetails } from './ProductDetailPage';
+export { default as ProductCard } from './ProductCard';
+export { default as ProductFilters } from './ProductFilters';
+export { default as ProductSkeleton } from './ProductSkeleton';
+export { default as AnimatedSection } from './AnimatedSection';
+export { default as productsData } from './productsData';
