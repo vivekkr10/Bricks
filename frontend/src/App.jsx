faad9520a15662e247ffe3b1194a1f4c673689ce
Palak-72 <palakg7848@gmail.com>
2026-02-20 19:09:09 +0530

    Admin Dashboard updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import ProductForm from "./pages/AdminDashboard/ProductForm.jsx"
 import Profile from "./pages/AdminDashboard/profile.jsx"
 
 
+
 // 3. The Temporary Fix
 import ComingSoon from "./Components/commingsoon.jsx";
 import { ProductsPage, ProductDetails } from "./pages/Products";
@@ -43,7 +44,7 @@ function App() {
         {/* Admin Auth Routes */}
         <Route path="/dashboard" element={<Dashboard/>} /> 
          <Route path="/profile" element={<Profile/>} /> 
-        <Route path="/ ProductForm" element={<ProductForm/>} />
+        <Route path="/ProductForm" element={<ProductForm/>} />
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/admin-register" element={<AdminRegister />} />
         {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
