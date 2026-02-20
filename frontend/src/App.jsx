8223b51e91120ebd1fbf0bc3a0749e19dadcf686
Vedantkale01 <vedantramdaskale@gmail.com>
2026-02-20 19:29:47 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,16 +14,8 @@ import Profile from "./pages/AdminDashboard/profile.jsx"
 
 // 3. The Temporary Fix
 import ComingSoon from "./Components/commingsoon.jsx";
-<<<<<<< aboutChangesAndProjectChanges
-import ProductsPage from "./pages/Products/ProductsPage.jsx"
-import ProductDetails from "./pages/Products/ProductDetailPage.jsx";
-import BlogSection from "./pages/blog/BlogSection.jsx";
-import Contact from "./pages/contactPage/Contact.jsx";
-import BlogDetails from "./pages/blog/BlogDetails.jsx";
-=======
 import { ProductsPage, ProductDetails } from "./pages/Products";
 import Contact from "./pages/contactPage/ContactSection.jsx";
->>>>>>> main
 import AboutPage from "./pages/AboutPage/About.jsx";
 // Projects Page
 import ProjectPage from "./pages/Project/Project.jsx";
@@ -64,15 +56,8 @@ function App() {
         {/* <Route path="/admin-register" element={<AdminRegister />} /> */}
 
         <Route path="/products" element={<ProductsPage />} />
-<<<<<<< aboutChangesAndProjectChanges
-        <Route path="/product/:id" element={<ProductDetails />} />
-
-        <Route path="/blog" element={<BlogSection />} />
-        <Route path="/blog/:id" element={<BlogDetails />} />
-=======
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/blog" element={<FeaturedArticle />} />
->>>>>>> main
         <Route path="/contact" element={<Contact />} />
         <Route path="/inquiry" element={<Inquiry />} />
         <Route path="/terms" element={<Terms />} />
